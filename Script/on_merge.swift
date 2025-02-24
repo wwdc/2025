ffd983045597465ad482d3019571191176fbc650
Piotr Jeremicz <piotrek@jeremicz.com>
2025-02-24 10:56:24 +0100

    Updated script

--- Script/on_merge.swift
+++ Script/on_merge.swift
@@ -32,7 +32,7 @@ EXAMPLE
 Name<required>: John Appleseed
 Status<required>: Submitted <or> Winner <or> Distinguished <or> Rejected
 ProjectTitle: The Accessibility Rose
-Technologies<4 maximum>: SwiftUI, RealityKit, CoreGraphic 
+Technologies<only the first 4 are visible>: SwiftUI, RealityKit, CoreGraphic 
 
 AboutMeUrl: https://linkedin.com/in/johnappleseed <
 SourceUrl: https://github.com/johnappleseed/wwdc2025
@@ -163,7 +163,7 @@ struct Submission {
             "-"
         }
         
-        let technologiesRow = technologies.joined(separator: ", ")
+        let technologiesRow = technologies.prefix(4).joined(separator: ", ")
         
         let statusRow: String = "![\(status.rawValue)](\(status.iconURLString))"
         
